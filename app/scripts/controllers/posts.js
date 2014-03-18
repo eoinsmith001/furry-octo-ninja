9dd9ac58851d985e4cae38b6dc212e50a73d9fd2
Eoin Smith <eoinsmith001@gmail.com>
2014-03-18 15:59:09 +1000

    adding and deleting of posts

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,6 +1,17 @@
 'use strict';
 
 app.controller( 'PostsCtrl', function($scope) {
-  // post consists of url and title
+  // post object consists of url and title
   $scope.post = { url : "http://", title : '' };
+  $scope.posts = [];
+
+  $scope.submitPost = function() {
+    $scope.posts.push( $scope.post );
+    $scope.post = { url : "http://", title : '' };
+  };
+
+  $scope.deletePost = function(index) {
+    console.log("delete from index " + index );
+    $scope.posts.splice( index, 1 );
+  };
 });
