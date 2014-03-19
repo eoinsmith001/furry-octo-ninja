6f319d6e033577cbf29efe98196b9d20553a52c2
Eoin Smith <eoinsmith001@gmail.com>
2014-03-19 15:32:22 +1000

    added comment page primitive and redirect functionality when submitting post

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,13 +1,13 @@
 'use strict';
 
-app.controller( 'PostsCtrl', function($scope, Post) {
+app.controller( 'PostsCtrl', function($scope, $location, Post) {
   // post object consists of url and title
   $scope.posts = Post.all;
   $scope.post = { url : 'http://', title : '' };
 
   $scope.submitPost = function() {
-    Post.create( $scope.post ).then( function() {
-      $scope.post = { url : 'http://', title : '' };
+    Post.create( $scope.post ).then( function(ref) {
+      $location.path('/posts/' + ref.name());
     });
     $scope.post = { url : 'http://', title : '' };
   };
