4435c6f6518554ad32753f6e6814d10b8d77495f
Eoin Smith <eoinsmith001@gmail.com>
2014-03-28 15:05:20 +1000

    comment add and browse functionality added

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -32,6 +32,16 @@ app.factory( 'Post',
         }
         return posts.$remove(postId);
       },
+      addComment : function( postId, comment ) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+          comment.username = user.username;
+          comment.postId = postId;
+          posts.$child(postId).$child('comments').$add(comment).then(function(ref){
+            user.$child('comments').$child(ref.name()).$set({id: ref.name(), postId: postId});
+          });
+        }
+      }
     };
     return Post;
   });
