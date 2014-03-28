1e5d49ee2084586d01bb6bcb17e4a21733f59b60
Eoin Smith <eoinsmith001@gmail.com>
2014-03-28 14:28:02 +1000

    integrate post service with User service

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 app.controller( 'NavCtrl', function($scope, $location, Post, Auth) {
   $scope.post = {url:'http://', title:''};
 
@@ -9,9 +8,9 @@ app.controller( 'NavCtrl', function($scope, $location, Post, Auth) {
   };
 
   $scope.submitPost = function() {
-    Post.create( $scope.post ).then(function(ref) {
-      $location.path('/posts/'+ref.name() );
+    Post.create( $scope.post ).then(function(postId) {
       $scope.post = {url:'http://', title:''};
+      $location.path('/posts/'+ postId );
     });
   };
 });
